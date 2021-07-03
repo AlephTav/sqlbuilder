488ed5fa45d5f6dae4d463eb33982dcc03b56aa3
Sergey Milimko <smilimko@gmail.com>
2021-07-03 15:04:42 +0500

    Adjust code to psalm level 1

--- src/Sql/Execution/DataFetching.php
+++ src/Sql/Execution/DataFetching.php
@@ -8,9 +8,6 @@ use RuntimeException;
 
 trait DataFetching
 {
-    /**
-     * @psalm-return list<array<string,mixed>>
-     */
     public function rows(): array
     {
         return $this->db()->rows($this->toSql(), $this->getParams());
